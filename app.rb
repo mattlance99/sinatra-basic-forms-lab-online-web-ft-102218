c7c6b27b1460a19f669168f4802c3fd6c47c9de9
Matthew Cohen <matt.cohen182@gmail.com>
2018-12-16 18:23:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ class App < Sinatra::Base
     @name = params[:name]
     @breed = params[:breed]
     @age = params[:age]
-    binding.pry
     @puppy = Puppy.new(@name, @breed, @age)
+        binding.pry
     erb :display_puppy
   end
 
